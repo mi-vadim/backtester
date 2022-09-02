ea7a2071c80c0684d16c04cd4d5ae8042a4dba2a
Milevskiy Vadim <37630058+mi-vadim@users.noreply.github.com>
2022-09-03 01:45:41 +0300

    Fix backtesting

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ except ValueError:
 if period > 0 and multiplier > 0:
     backtester.super_trend(period=period, multiplier=multiplier, investment=1000, use_tp=False)
 else:
-    for period, multiplier in [(x, y / 10) for x in range(1, 2) for y in range(10, 11)]:
+    for period, multiplier in [(x, y / 10) for x in range(1, 15) for y in range(10, 120)]:
         backtester.super_trend(period=period, multiplier=multiplier, investment=1000, use_tp=False)
     print(backtester.top_results())
 
